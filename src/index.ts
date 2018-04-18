6b7ccb331a799dc2d17a3d53f5b5271f2eb978c2
jpmonette <contact@jpmonette.net>
2018-04-18 13:57:48 +0100

    Replace query builder to salesforce-queries

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import Q from './q';
+import { SOQL } from 'salesforce-queries';
 
-function getFields(info) {
-  const fields: string[] = [];
-  info.fieldNodes.map(fieldNode => {
+function getFields(info: any) {
+  const fields: Set<string> = new Set([]);
+  info.fieldNodes.map((fieldNode: any) => {
     if (fieldNode.selectionSet) {
-      fieldNode.selectionSet.selections.map(value => {
-        if (!value.selectionSet) fields.push(value.name.value);
+      fieldNode.selectionSet.selections.map((value: any) => {
+        if (!value.selectionSet) fields.add(value.name.value);
       });
     }
   });
-  return fields;
+  return [...fields];
 }
 
-function getWheres(info): any[] {
+function getWheres(info: any): any[] {
   const wheres: { field: string; value: string; operator: string }[] = [];
-  info.fieldNodes.map(fieldNode => {
-    fieldNode.arguments.map(val => {
+  info.fieldNodes.map((fieldNode: any) => {
+    fieldNode.arguments.map((val: any) => {
       const field = val.name.value;
       const value = val.value.value;
       if (field !== 'limit') {
@@ -26,10 +26,10 @@ function getWheres(info): any[] {
   return wheres;
 }
 
-function getLimit(info): number | void {
+function getLimit(info: any): number | void {
   let limit;
-  info.fieldNodes.map(fieldNode => {
-    fieldNode.arguments.map(value => {
+  info.fieldNodes.map((fieldNode: any) => {
+    fieldNode.arguments.map((value: any) => {
       if (value.name.value === 'limit') {
         limit = value.value.value;
       }
@@ -38,17 +38,21 @@ function getLimit(info): number | void {
   return limit;
 }
 
+export interface Options {
+  conn: any;
+}
+
 class Salesforce {
   conn: any;
 
-  constructor(props) {
-    console.log('CONSTRUCTOR')
+  constructor(props: Options) {
     this.conn = props.conn;
   }
 
-  public query = (parent: { key: string }, info) => {
-    console.log('BEFORE')
-    const queryBuilder = new Q(info.returnType.ofType || info.returnType).select(getFields(info));
+  public query = (parent: { [key: string]: string }, info: any) => {
+    const queryBuilder = new SOQL(info.returnType.ofType || info.returnType).select(
+      getFields(info)
+    );
     const limit = getLimit(info);
 
     if (limit) {
@@ -60,7 +64,6 @@ class Salesforce {
     wheres.map(({ field, operator, value }) => queryBuilder.where(field, operator, value));
     Object.keys(parent).map((key: string) => queryBuilder.where(key, '=', parent[key]));
     const query = queryBuilder.build();
-    console.log('\nExecuting', query);
     return this.conn.query(query);
   };
 }
